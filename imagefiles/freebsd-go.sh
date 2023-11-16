122e1c6221b6b4d13301d13d134844701a2350e6
cynix <cynix@cynix.org>
2023-11-16 19:47:54 +1100

    Update Go to v1.21.4

--- imagefiles/freebsd-go.sh
+++ imagefiles/freebsd-go.sh
@@ -4,4 +4,4 @@ set -e
 set -o pipefail
 
 mkdir -p /usr/local
-curl -sSfL https://go.dev/dl/go1.20.5.linux-amd64.tar.gz | tar -C /usr/local/ -xzf-
+curl -sSfL https://go.dev/dl/go1.21.4.linux-amd64.tar.gz | tar -C /usr/local/ -xzf-
