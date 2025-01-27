930f2a1d1aa08cea1c907b21b8ec731cdcf3b0ce
cynix <cynix@cynix.org>
2025-01-27 17:50:37 +1100

    Add rust nightly target

--- imagefiles/freebsd-rust.sh
+++ imagefiles/freebsd-rust.sh
@@ -6,6 +6,8 @@ set -o pipefail
 curl -sSfL https://sh.rustup.rs | sh -s -- -y --default-toolchain none --no-modify-path
 
 rustup toolchain install stable
-rustup target add --toolchain stable x86_64-unknown-freebsd
-
 rustup toolchain install nightly --component rust-src
+
+for toolchain in stable nightly; do
+  rustup target add --toolchain $toolchain x86_64-unknown-freebsd
+done
