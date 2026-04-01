9c32f366f999ab64f9786bc45bd69613179706a9
Julian Oes <julian@oes.ch>
2026-04-02 12:50:40 +1300

    Fix CMAKE_CROSSCOMPILING_EMULATOR path to use qemu-aarch64-static
    AlmaLinux 9 doesn't package qemu-user-static, so the Dockerfile
    downloads the static binary as /usr/bin/qemu-aarch64-static.
    The Toolchain.cmake was incorrectly referencing /usr/bin/qemu-aarch64.

--- manylinux_2_34-aarch64/Toolchain.cmake
+++ manylinux_2_34-aarch64/Toolchain.cmake
@@ -18,4 +18,4 @@ set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
 set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
 set(CMAKE_SYSROOT ${cross_root}/${cross_triple}/sysroot)
 
-set(CMAKE_CROSSCOMPILING_EMULATOR /usr/bin/qemu-aarch64)
+set(CMAKE_CROSSCOMPILING_EMULATOR /usr/bin/qemu-aarch64-static)
